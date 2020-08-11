3e78931b211bb2348c84e0039919899ab3ecae54
samuelematias <hi@samuelematias.com>
2020-08-11 11:15:29 -0300

    feat(locator): register DialogService on locator

--- lib/locator.dart
+++ lib/locator.dart
@@ -1,5 +1,8 @@
+import 'package:dialog_manager/services/dialog_service.dart';
 import 'package:get_it/get_it.dart';
 
 GetIt locator = GetIt.instance;
 
-void setupLocator() {}
+void setupLocator() {
+  locator.registerLazySingleton(() => DialogService());
+}
