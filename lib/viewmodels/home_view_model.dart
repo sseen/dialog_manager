c09859385fe2f9fdbadb86ba530a72274f07a11b
samuelematias <hi@samuelematias.com>
2020-08-11 11:14:53 -0300

    feat(viewmodel): improving HomeViewModel

--- lib/viewmodels/home_view_model.dart
+++ lib/viewmodels/home_view_model.dart
@@ -1,5 +1,13 @@
+import 'package:dialog_manager/locator.dart';
+import 'package:dialog_manager/services/dialog_service.dart';
 import 'package:flutter/foundation.dart';
 
 class HomeViewModel extends ChangeNotifier {
-  Future doThings() async {}
+  DialogService _dialogService = locator<DialogService>();
+
+  Future doThings() async {
+    print('dialog called');
+    var dialogResult = await _dialogService.showDialog();
+    print('dialog closed');
+  }
 }
