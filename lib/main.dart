ba21d4725260a6440c4cf2fcdc850b9a3c5e10c2
samuelematias <hi@samuelematias.com>
2020-08-11 11:16:03 -0300

    feat(main): set DialogManager on main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:dialog_manager/locator.dart';
+import 'package:dialog_manager/managers/dialog_manager.dart';
 import 'package:dialog_manager/ui/home/home_view.dart';
 import 'package:flutter/material.dart';
 
@@ -8,11 +9,17 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      builder: (context, widget) => Navigator(
+        onGenerateRoute: (settings) => MaterialPageRoute(
+          builder: (context) => DialogManager(
+            child: widget,
+          ),
+        ),
+      ),
+      title: 'Dialog Manager Setup',
       home: HomeView(),
     );
   }
